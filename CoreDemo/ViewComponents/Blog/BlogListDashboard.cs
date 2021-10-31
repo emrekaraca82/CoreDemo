e2df27a867f035d1f020e34c76b9c7df3dd86c77
Emre Karaca <53429328+emrekaraca82@users.noreply.github.com>
2021-11-01 00:04:36 +0300

    01.11.2021

---
+++ CoreDemo/ViewComponents/Blog/BlogListDashboard.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.ViewComponents.Blog
+{
+    public class BlogListDashboard : ViewComponent
+    {
+        BlogManager bm = new BlogManager(new EfBlogDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = bm.GetBlogListWithCategory();
+            return View(values);
+        }
+    }
+}
