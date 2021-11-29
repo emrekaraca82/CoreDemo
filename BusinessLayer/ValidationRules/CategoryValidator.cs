0accbaf8990b333d9591807d311fe7b9e952087c
Emre Karaca <53429328+emrekaraca82@users.noreply.github.com>
2021-11-29 23:59:39 +0300

    29.11.2021

---
+++ BusinessLayer/ValidationRules/CategoryValidator.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class CategoryValidator: AbstractValidator<Category>
+    {
+        public CategoryValidator()
+        {
+            RuleFor(x => x.CategoryName).NotEmpty().WithMessage("Kategori adı boş geçilemez");
+            RuleFor(x => x.CategoryDescription).NotEmpty().WithMessage("Kategori içerik boş geçilemez");        
+            RuleFor(x => x.CategoryDescription).MaximumLength(150).WithMessage("150 Krakterden az başlık giriniz");
+            RuleFor(x => x.CategoryDescription).MinimumLength(4).WithMessage("4 en az krakterden başlık giriniz");
+        }
+    }
+}
--- CoreDemo/Areas/Admin/Controllers/CategoryController.cs
+++ CoreDemo/Areas/Admin/Controllers/CategoryController.cs
@@ -1,17 +1,96 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.Concrete.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using X.PagedList;
 
 namespace CoreDemo.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+        public IActionResult Index(int page = 1)
+        {
+            var values = cm.GetAll().ToPagedList(page,pageSize:10);
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddCategory()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult AddCategory(Category par )
+        {
+            CategoryValidator cv = new CategoryValidator();
+            ValidationResult results = cv.Validate(par);
+            if (results.IsValid)
+            {
+                par.CategoryStatus = true;
+                par.CreateDate = DateTime.Parse(DateTime.Now.ToShortDateString());            
+                cm.Add(par);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+        public IActionResult DeleteCategory(int id)
+        {
+            var value = cm.GetById(id);
+            if (value.CategoryStatus == true)
+            {
+                value.CategoryStatus = false;
+            }
+            else
+            {
+                value.CategoryStatus = true;
+            }
+            cm.Update(value);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateCategory(int id)
+        {
+            var values = cm.GetById(id);
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateCategory(Category par)
+        {
+            CategoryValidator cv = new CategoryValidator();
+            ValidationResult results = cv.Validate(par);
+            if (results.IsValid)
+            {
+                par.CategoryStatus = true;
+                cm.Update(par);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+
     }
 }
