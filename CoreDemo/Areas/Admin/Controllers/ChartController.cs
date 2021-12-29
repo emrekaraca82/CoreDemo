5ba6a81f94777de11936b210101c83ed53af4f3a
Emre Karaca <53429328+emrekaraca82@users.noreply.github.com>
2021-12-30 02:04:41 +0300

    30.12.2021 commit

---
+++ CoreDemo/Areas/Admin/Controllers/ChartController.cs
@@ -0,0 +1,33 @@
+﻿using CoreDemo.Areas.Admin.Models;
+using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ChartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CategoryChart()
+        {
+            List<CategoryClass> list = new List<CategoryClass>();
+            using (var c = new Context())
+            {
+                list = c.Categories.Select(x => new CategoryClass
+                {
+                    categoryName = x.CategoryName,
+                    categoryCount = x.Blogs.Count()
+                }).ToList();
+            }          
+            return Json(new { jsonlist = list });
+        }
+    }
+}
